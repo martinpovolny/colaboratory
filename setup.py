1949780e77f5ea6c82e3ad77ab445a426c7365a7
Martin Povolny <mpovolny@redhat.com>
2020-11-05 21:49:57 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+__version__ = "0.2"
+
+setup(
+    name="src",
+    packages=find_packages(),
+    version=__version__,
+    description="notebooks from the colaboratory",
+    author="martin povolny",
+    license="",
+    install_requires=[],
+)
